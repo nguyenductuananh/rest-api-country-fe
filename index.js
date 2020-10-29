3925002e4f3f4b987788355ebaab5fc97e89d1dc
nguyenductuananh <nguyentuananh.phuphuc@gmail.com>
2020-10-29 21:23:37 +0700

    Darkmode for homepage

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let selectBtn = document.getElementsByClassName('custom-select__trigger')[0];
 let optionDropdown = document.getElementsByClassName('custom-options')[0];
-
+let isDarkmode = false;
 let isHidden = true;
 selectBtn.addEventListener('click', showDropdown);
 function showDropdown(){
@@ -18,3 +18,28 @@ function showDropdown(){
     }
 }
 
+function changeMode(){
+    if(!isDarkmode){
+        document.getElementsByTagName('body')[0].classList.add('dark');
+        document.getElementsByTagName('header')[0].classList.add('dark');
+        document.getElementsByClassName('search')[0].classList.add('dark');
+        document.getElementsByClassName('searchInput')[0].classList.add('dark');
+        document.getElementsByClassName('region')[0].classList.add('dark');
+        document.getElementsByClassName('custom-options')[0].classList.add('dark');
+        for(let nation of document.getElementsByClassName('country')) nation.classList.add('dark');
+        document.getElementsByClassName('arrow')[0].classList.add('dark');
+        document.getElementsByClassName('dark-mode-btn')[0].classList.add('dark');
+        isDarkmode = true;
+    } else {
+        isDarkmode = false;
+        document.getElementsByTagName('body')[0].classList.remove('dark');
+        document.getElementsByTagName('header')[0].classList.remove('dark');
+        document.getElementsByClassName('search')[0].classList.remove('dark');
+        document.getElementsByClassName('searchInput')[0].classList.remove('dark');
+        document.getElementsByClassName('region')[0].classList.remove('dark');
+        document.getElementsByClassName('custom-options')[0].classList.remove('dark');
+        for(let nation of document.getElementsByClassName('country')) nation.classList.remove('dark');
+        document.getElementsByClassName('arrow')[0].classList.remove('dark');
+        document.getElementsByClassName('dark-mode-btn')[0].classList.remove('dark');
+    }
+}
